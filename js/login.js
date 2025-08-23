5266e07cbb996b15d251613596f602f59ef83783
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-24 03:31:05 +0600

    Handle Login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -1,19 +1,50 @@
 // console.log('button clicking file added');
 
 // document.getElementById('button_login').addEventListener('click', function (){
-    
+
 // })
 
 // Search: from submit reloading the page
 
 // step - 1 set event handler
-document.getElementById('button_login').addEventListener('click', function(event){
-    // step-2: prevent default behavior (prevent reloading browser)
-    event.preventDefault();  // <-------- vejal to beginners
-    console.log('login button clicked');
+// document.getElementById('button_login').addEventListener('click', function (event) {
+//     // step-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault();  // <-------- vejal to beginners
 
-    // step-3: get the phone number
+//     // step-3: get the phone number and pin number
+//     const phoneNumber = document.getElementById('phone_number').value;
+//     const pinNumber = document.getElementById('pin_number').value;
+//     console.log(phoneNumber, pinNumber);
+
+//     // step-4: validate phone and pin
+//     // this is temporary. you should not do like this
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('you are logged in');
+//         // step -5: allow user to use the website
+//     }
+//     else{
+//         alert('wrong phone number or pin');
+//     }
+// })
+
+
+document.getElementById('button_login').addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('button clicked');
+
+    // get phone number and pin
     const phoneNumber = document.getElementById('phone_number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin_number').value;
+    console.log(phoneNumber, pinNumber);
     
+    // bad way to validate
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html';
+        
+    }
+    else{
+        console.log('wrong phone number or pin.');
+        
+    }
 })
