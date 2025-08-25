64002ed34b256a1aec41efa5806ec92967c677ef
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-08-25 23:59:59 +0600

    Verify Pin Number and add money to the balance

--- js/home.js
+++ js/home.js
@@ -4,6 +4,9 @@
  * S-1: add event handler
  * prevent page reload after form submit
  * S-2: get money to be added to the account balance
+ *  get the pin number
+ * 
+ * S-3: verify the pin number
  */
 // step-1: add an event handeler to add money button inside the form
 document.getElementById('btn_add_money').addEventListener('click', function (event) {
@@ -17,5 +20,28 @@ document.getElementById('btn_add_money').addEventListener('click', function (eve
     // get the pin number provided
     const pinNumberInput = document.getElementById('input_pin_number').value;
     console.log(pinNumberInput);
-    
+
+    // step-3: verify pin number
+    // wrong way to validate pin number
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+
+        // steo-4: get the current balance
+        const balance = document.getElementById('account_balance').innerText;
+        console.log(typeof balance);
+
+        // step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber ;
+        console.log(newBalance);
+                
+       // step-6: update the balance in the UI/DOM
+       document.getElementById('account_balance').innerText = newBalance;
+        
+    }
+    else{
+        
+        alert('Failed to add money! Please try again.');
+    }
 })
